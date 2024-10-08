6bfff165debff913ca6a2119f855917f70e29bc7
IshqhsI <oneridh18@gmail.com>
2024-10-08 15:34:44 +0800

    update route web: add prefix and name for route admin

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
+Route::prefix('/admin')->name('admin.')->middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
