0ff54f2c2790d6f430e034f8664a49e06f9c6a62
IshqhsI <oneridh18@gmail.com>
2024-10-08 14:30:53 +0800

    add migration for table items

---
+++ database/migrations/2024_10_08_132627_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('features')->nullable();
+            $table->text('images')->nullable();
+            $table->integer('price')->default(0);
+            $table->double('stars')->default(0);
+            $table->double('reviews')->default(0);
+            $table->foreignId('type_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('brand_id')->nullable()->constrained()->nullOnDelete();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
