dd0aabf79ce9d5b28e592838adf34f4f0776520e
IshqhsI <oneridh18@gmail.com>
2024-10-08 15:40:42 +0800

    update route for dashboard & change route name in navbar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,7 +12,5 @@ Route::prefix('/admin')->name('admin.')->middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
 });
