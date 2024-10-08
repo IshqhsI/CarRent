14eb64e56997e5d18cc80fc8245bfc98ea75ac9c
IshqhsI <oneridh18@gmail.com>
2024-10-08 15:31:24 +0800

    update item model: add relation to booking

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -18,4 +18,8 @@ class Item extends Model
     public function type(){
         return $this->belongsTo(Type::class);
     }
+
+    public function bookings(){
+        return $this->hasMany(Booking::class);
+    }
 }
