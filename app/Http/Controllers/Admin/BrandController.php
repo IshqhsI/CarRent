b8cd3c77dd8c941b54650f371882a3c09d7fc081
IshqhsI <oneridh18@gmail.com>
2024-10-09 11:30:32 +0800

    add brand controller

---
+++ app/Http/Controllers/Admin/BrandController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Brand;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+use App\Http\Requests\BrandRequest;
+use App\Http\Controllers\Controller;
+
+class BrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Datatables
+        if (request()->ajax()) {
+            $query = Brand::query();
+
+            // with tailwind
+            return DataTables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                        <div class="flex justify-center">
+                            <a href="' . route('admin.brand.edit', $item->id) . '" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">Edit</a>
+                            <form action="' . route('admin.brand.destroy', $item->id) . '" method="POST">
+                                ' . method_field('delete') . csrf_field() . '
+                                <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+                            </form>
+                        </div>
+                    ';
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+
+        return view('admin.brand.index');
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.brand.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(BrandRequest $request)
+    {
+        $data = $request->all();
+        $data['slug'] = Str::lower(Str::slug($request->name) . '-' . Str::random(5));
+        Brand::create($data);
+        return redirect()->route('admin.brand.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
