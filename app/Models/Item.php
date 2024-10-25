0caeceb999609e97dbbdad9502b600ffcefa17fc
IshqhsI <oneridh18@gmail.com>
2024-10-25 09:16:19 +0800

    update item model: add casting images & add  method to get thumbnail attribute

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,14 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Item extends Model
 {
     use HasFactory, SoftDeletes;
     protected $guarded = ['id'];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function getThumbnailAttribute(){
+        if($this->images){
+            return Storage::url(json_decode($this->images)[0]);
+        }
+        return asset('images/default.png');
+    }
 
     public function brand(){
         return $this->belongsTo(Brand::class);
