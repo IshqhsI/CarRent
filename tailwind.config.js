d3536927319b86e487b18df04e3760104456b6a5
IshqhsI <oneridh18@gmail.com>
2024-10-26 18:33:06 +0700

    update tailwind and vite config & add resource css front

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,33 @@ import typography from '@tailwindcss/typography';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "1rem",
+                lg: "75px",
+                xl: "100px",
+            },
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+                poppins: ["Poppins"],
+            },
+            colors: {
+                primary: "#4743FB",
+                secondary: "#9D9DBC",
+                dark: "#0D0C41",
+                grey: "#D8D8E4",
+                darkGrey: "#F5F6F6",
+                subtlePars: "#B0AED6",
             },
         },
     },
