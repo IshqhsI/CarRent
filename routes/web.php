cfda071585f6221861f595ed8187fa3a095c0293
IshqhsI <oneridh18@gmail.com>
2024-11-06 17:32:59 +0800

    update route: renaming home to front group

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
-use App\Http\Controllers\Admin\BrandController as AdminBrandController;
-use App\Http\Controllers\Admin\TypeController as AdminTypeController;
+use App\Http\Controllers\Front\LandingController;
 use App\Http\Controllers\Admin\ItemController as AdminItemController;
+use App\Http\Controllers\Admin\TypeController as AdminTypeController;
+use App\Http\Controllers\Admin\BrandController as AdminBrandController;
 use App\Http\Controllers\Admin\BookingController as AdminBookingController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name('front.')->group(function () {
+    Route::get('/', [LandingController::class, 'index'])->name('index');
 });
 
 Route::prefix('/admin')->name('admin.')->middleware([
     'auth:sanctum',
-    config('jetstream.auth_session'),
     'verified',
+    config('jetstream.auth_session'),
+    config('jetstream.is_admin'),
 ])->group(function () {
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
     Route::resource('/brand', AdminBrandController::class);
